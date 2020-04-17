664ee0c949226814c8c831e075b60eb02f1990d2
mryang113 <mryang113@163.com>
2020-04-17 20:51:23 +0800

    vue_项目2期初始化结构

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,24 @@
 <template>
   <div id="app">
-    <ele-header></ele-header>
-    <div class="navs">
-      <div class="item">
-        <router-link to="/goods">商品</router-link>
-      </div>
-      <div class="item">
-        <router-link to="/ratings">评价</router-link>
-      </div>
-      <div class="item">
-        <router-link to="/seller">商家</router-link>
-      </div>
-    </div>
-    <keep-alive>
-      <router-view></router-view>
-    </keep-alive>
+    <router-view></router-view>
+    <FooterGuide></FooterGuide>
   </div>
 </template>
 
 <script>
-  import header from "components/ele-header/ele-header.vue";
-  import {mapActions} from "vuex";
-  import {GETSELLER} from "store/mutation_types"
+  import FooterGuide from 'components/FooterGuide/FooterGuide'
   export default {
-    name: 'App',
+    name: "app",
     components:{
-        "ele-header":header
-    },
-    methods:{
-        ...mapActions([GETSELLER])
-    },
-    mounted(){
-        this[GETSELLER]()
+      FooterGuide
     }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "./common/stylus/mixin.styl";
   #app
     position relative
     width 100%
     height 100%
     overflow hidden
-    .navs
-      one-px(black)
-      display flex
-      height 40px
-      & > .item
-        flex 1
-        a
-          center()
-          width 100%
-          height 100%
-          font-size 14px
-          line-height 14px
-          color rgba(77,85,93,1)
-          &.active
-            color rgba(240,20,20,1)
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,31 @@
 //__dirname 绝对路径;代表的是当前文件的目录
 const path = require('path')
-function resolve (dir) {
-    return path.join(__dirname, dir)
+function resolve(dir) {
+  return path.join(__dirname, dir)
 }
 
-module.exports={
-    lintOnSave:false,
-    devServer:{
-       open:true
-    },
-    configureWebpack:{
-        resolve: {
-            alias: {
-                'components': resolve('src/components'),
-                'pages': resolve('src/pages'),
-                'store': resolve('src/store'),
-            }
+module.exports = {
+  lintOnSave: false,
+  devServer: {
+    open: true,
+    // proxy: 'http://localhost:4000' ,//vue-cli_代理的配置
+    proxy: {
+      '/4000': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        pathRewrite: {
+          "^/4000": ""
         }
+      }
     }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'components': resolve('src/components'),
+        'pages': resolve('src/pages'),
+        'store': resolve('src/store'),
+      }
+    }
+  }
 }
