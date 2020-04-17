fd9203a2c60238b8e432ec593bfa4f337b74c39f
mryang113 <mryang113@163.com>
2020-04-17 22:45:51 +0800

    03a_编程时导航

---
+++ src/http/wrap/config.js
@@ -0,0 +1,10 @@
+export default {
+  name:"wrap",
+  // baseUrl:"http://localhost:4000",
+  api:{
+      getPosition:{
+          url:"/4000/position/40.10038,116.36867",
+          method:"get"
+      }
+  }
+}
--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,23 @@ const path = require('path')
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
+// http://localhost:8080/css/reset.css 这个路径是可以在devServer中访问到确切的静态资源的
+// http://localhost:8080/position/40.10038,116.36867 这个路径在devServer中是不存在对应的静态资源的 404请求
 
 module.exports = {
   lintOnSave: false,
+
+  //devServer 项目中的静态资源服务器  等同于生产环境中ngix!
+    //如果我们向devServer发送一个未知的http请求!!!  而且这个请求在devServer中没有命中任何静态资源
+    //devServe会返回index.html  因为这个http请求有可能是前端路由 不是去访问静态资源的
+    //devServe返回index.html之后 再去请求打包成功之后的js文件 在那个js文件中根据vue-router的配置
+    //来显示对应的界面
+    //如果我们向devServer发送一个ajax请求 而且devserver为当前这个ajax请求配置了代理 代理到真正
+    // 的api服务器上去的时候  发现这个api服务器没有对应的后台路由 这个时候devServer会认为自己已经
+    // 处理成功 只是api服务器没有成功  那devsever 会将index,html返回回去
+    
   devServer: {
+    /****** 在vue_a.md上有详细文档0417  ******* */
     open: true,
     // proxy: 'http://localhost:4000' ,//vue-cli_代理的配置
     proxy: {
